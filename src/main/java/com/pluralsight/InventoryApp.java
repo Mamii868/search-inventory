bc819b0889d2bf59421d11ea4342a2ddd00fa768
Mamii868 <marcmotenii@gmail.com>
2026-04-22 15:13:43 -0500

    Add sorting

--- src/main/java/com/pluralsight/InventoryApp.java
+++ src/main/java/com/pluralsight/InventoryApp.java
@@ -3,13 +3,18 @@ package com.pluralsight;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class InventoryApp {
     public static void main(String[] args) {
 
         ArrayList<Product> inventory = getInventory();
 
+//        Sort inventory by name
+        inventory.sort(Comparator.comparing(Product::getName));
+
         System.out.println("We carry the following inventory: ");
+
 
         for (Product product : inventory) {
             System.out.printf("id: %d, %s - $%.2f \n", product.getId(), product.getName(), product.getPrice());
@@ -18,7 +23,7 @@ public class InventoryApp {
     }
 
     public static ArrayList<Product> getInventory() {
-        ArrayList<Product> productList = new ArrayList<Product>();
+        ArrayList<Product> productList = new ArrayList<>();
         try {
             FileReader fileReader = new FileReader("src/main/resources/inventory.csv");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
