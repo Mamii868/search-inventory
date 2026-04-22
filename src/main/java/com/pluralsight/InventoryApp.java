ccbf94ffbe89848b2281b9ecb6143f10430336cb
Mamii868 <marcmotenii@gmail.com>
2026-04-22 14:26:25 -0500

    Add inventory

--- src/main/java/com/pluralsight/InventoryApp.java
+++ src/main/java/com/pluralsight/InventoryApp.java
@@ -1,7 +1,22 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class InventoryApp {
     public static void main(String[] args) {
 
+
+    }
+
+    public static ArrayList<Product> getInventory() {
+        ArrayList<Product> productList = new ArrayList<Product>();
+
+        productList.add(new Product(1, "AMD Ryzen 5600x", 156.99));
+        productList.add(new Product(2, "Razer Chroma Cynosa", 49.99));
+        productList.add(new Product(3, "Steel Series X", 79.99));
+        productList.add(new Product(4, "Asus TUFF 240hz Monitor", 294.99));
+        productList.add(new Product(5, "CoolerMaster CPU cooler", 34.99));
+
+        return productList;
     }
 }
