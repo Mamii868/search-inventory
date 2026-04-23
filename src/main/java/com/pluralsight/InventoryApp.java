af709a88762d2fdbc914a564df3bca8e38831e5c
Mamii868 <marcmotenii@gmail.com>
2026-04-23 12:52:02 -0500

    Add menu

--- src/main/java/com/pluralsight/InventoryApp.java
+++ src/main/java/com/pluralsight/InventoryApp.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class InventoryApp {
     static Scanner scanner = new Scanner(System.in);
+    static boolean appRunning = true;
 
     public static void main(String[] args) {
 
@@ -15,7 +16,7 @@ public class InventoryApp {
 
 //        Sort inventory by name
         inventory.sort(Comparator.comparing(Product::getName));
-        mainMenu();
+        mainMenu(inventory);
 
     }
 
@@ -41,18 +42,32 @@ public class InventoryApp {
         return new Product(Integer.parseInt(splitFileData[0]), splitFileData[1], Double.parseDouble(splitFileData[2]));
     }
 
-    public static void mainMenu() {
-        System.out.println("=== Marc's Hardware(?) Store ===");
-        System.out.println("""
-                What do you want to do?
-                1- List all products
-                2- Lookup a product by its id
-                3- Find all products within a price range
-                4- Add a new product
-                5- Quit the application""");
-        System.out.print("Enter command");
-        String userInput = scanner.nextLine();
+    public static void mainMenu(ArrayList<Product> inventory) {
+        while (appRunning) {
+            System.out.println("=== Marc's Hardware(?) Store ===");
+            System.out.println("""
+                    What do you want to do?
+                    1- List all products
+                    2- Lookup a product by its id
+                    3- Find all products within a price range
+                    4- Add a new product
+                    5- Quit the application""");
+            System.out.print("Enter command: ");
+            int userInput = scanner.nextInt();
 
+            System.out.println();
+            switch (userInput) {
+                case 1 -> productList(inventory);
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void productList(ArrayList<Product> inventory) {
+        for (Product product : inventory) {
+            System.out.printf("id: %d, %s - $%.2f%n", product.getId(), product.getName(), product.getPrice());
+        }
 
     }
 }
